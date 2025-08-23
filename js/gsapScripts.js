3c0cadfbca19410024ea44226ff9cd0f3d332ba4
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-08-22 19:47:57 -0700

    bumped trigger start to 90%

--- js/gsapScripts.js
+++ js/gsapScripts.js
@@ -1,56 +1,50 @@
+// <!-- GSAP Animation -->
 
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(SplitText);
 
-    // <!-- GSAP Animation -->
+  let split = SplitText.create(".stagger", {
+    type: "chars",
+    mask: "chars",
+  });
+  let splitTwo = SplitText.create(".staggerTwo", {
+    type: "words",
+    mask: "words",
+  });
 
+  gsap.from(split.chars, {
+    duration: 0.2,
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05,
+  });
 
-      document.addEventListener("DOMContentLoaded", (event) => {
-        gsap.registerPlugin(ScrollTrigger);
-        gsap.registerPlugin(SplitText);
+  gsap.from(splitTwo.words, {
+    duration: 1.5,
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05,
+  });
 
-        let split = SplitText.create(".stagger", {
-          type: "chars",
-          mask: "chars",
-        });
-        let splitTwo = SplitText.create(".staggerTwo", {
-          type: "words",
-          mask: "words",
-        });
-
-        gsap.from(split.chars, {
-          duration: 0.2,
-          y: 100,
-          autoAlpha: 0,
-          stagger: 0.05,
-        });
-
-        gsap.from(splitTwo.words, {
-          duration: 1.5,
-          y: 100,
-          autoAlpha: 0,
-          stagger: 0.05,
-        });
-
-        gsap.utils.toArray(".fade-in").forEach((el) => {
-          gsap.fromTo(
-            el,
-            {
-              opacity: 0,
-              y: 50,
-            },
-            {
-              scrollTrigger: {
-                trigger: el,
-                start: "top 80%",
-                toggleActions: "play none none none",
-              },
-              opacity: 1,
-              y: 0,
-              duration: 1,
-              ease: "power2.out",
-            }
-          );
-        });
-      });
- 
-
-      
+  gsap.utils.toArray(".fade-in").forEach((el) => {
+    gsap.fromTo(
+      el,
+      {
+        opacity: 0,
+        y: 50,
+      },
+      {
+        scrollTrigger: {
+          trigger: el,
+          start: "top 90%",
+          toggleActions: "play none none none",
+        },
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power2.out",
+      }
+    );
+  });
+});
